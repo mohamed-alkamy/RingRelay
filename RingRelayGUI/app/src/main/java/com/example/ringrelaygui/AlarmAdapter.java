6a68b59b68243bb81618f966979e59bdf0d849a5
JoelAlgera <Joel.Algera@student.ufv.ca>
2025-03-01 12:28:25 -0800

    Alarm Deletion
    You can now add and delete alarms from the system database. I also started working on the settings page. Next step is to actually use the alarmItems as alarms in the system

--- RingRelayGUI/app/src/main/java/com/example/ringrelaygui/AlarmAdapter.java
+++ RingRelayGUI/app/src/main/java/com/example/ringrelaygui/AlarmAdapter.java
@@ -1,9 +1,11 @@
 package com.example.ringrelaygui;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
@@ -11,8 +13,11 @@ import android.widget.ArrayAdapter;
 import java.util.ArrayList;
 
 public class AlarmAdapter extends ArrayAdapter<AlarmEntity> {
+    private AlarmDatabase alarmDatabase;
+
     public AlarmAdapter(Context context, ArrayList<AlarmEntity> alarms) {
         super(context, 0, alarms);
+        alarmDatabase = AlarmDatabase.getInstance(context);
     }
 
     @Override
@@ -24,14 +29,26 @@ public class AlarmAdapter extends ArrayAdapter<AlarmEntity> {
         AlarmEntity alarm = getItem(position);
         TextView alarmTime = convertView.findViewById(R.id.alarmTimeText);
         Switch alarmSwitch = convertView.findViewById(R.id.alarmSwitch);
+        Button alarmDelete = convertView.findViewById(R.id.alarmDelete);
 
         if (alarm != null) {
             alarmTime.setText(alarm.getTime());
             alarmSwitch.setChecked(alarm.isEnabled());
+
+            // Handle Delete Button Click
+            alarmDelete.setOnClickListener(v -> deleteAlarm(position, alarm));
         }
 
         return convertView;
     }
+
+    private void deleteAlarm(int position, AlarmEntity alarm) {
+        AsyncTask.execute(() -> {
+            alarmDatabase.alarmDao().delete(alarm);
+            ((AlarmsFragment) ((MainActivity) getContext()).getSupportFragmentManager().findFragmentById(R.id.fragment_container)).removeAlarmFromList(position);
+        });
+    }
 }
+
 
 
